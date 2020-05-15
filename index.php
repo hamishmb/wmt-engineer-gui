cef68712d85e6d6e05275f1a41adfd5cc3f74cb1
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-05-15 13:46:51 +0100

    Quick check to make sure php is working

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--
+# Engineer GUI
+# Copyright (C) 2020 Wimborne Model Town
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 or,
+# at your option, any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<?php echo "<h1>Hello World</h1>"; ?>
