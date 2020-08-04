4b51bcd766aaed2513e4e32eb48c798826801f39
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-08-04 16:02:22 +0100

    Define version number as 1.0

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <?php
 
 // Engineer GUI
+// Version 1.0
 // Copyright (C) 2020 Wimborne Model Town
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License version 3 or,
